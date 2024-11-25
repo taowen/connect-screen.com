ffa4543cf9f5d53b5b9f8074c3adea4ef9d00d68
Tao Wen <taowen@gmail.com>
2024-11-25 13:12:19 +0800

    update

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -5,6 +5,7 @@ export const renderer = jsxRenderer(({ children }) => {
     <html>
       <head>
         <link href="/static/style.css" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
       </head>
       <body>{children}</body>
     </html>
